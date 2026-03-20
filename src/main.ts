a1f7c6b40ba320dc66e97a47f39d740775b599cc
Hiroto Sasaki <wmid23lim@gmail.com>
2026-03-21 00:17:45 +0900

    fix: 縁の描画

--- src/main.ts
+++ src/main.ts
@@ -1,12 +1,22 @@
-import { CANVAS_HEIGHT, CANVAS_WIDTH } from "./const"
+import { CANVAS_HEIGHT, CANVAS_WIDTH, DOLY_POLY_MAX_RADIUS } from "./const"
 
 const canvas = document.getElementById('canvas') as HTMLCanvasElement
 const context = canvas.getContext('2d')
 
+const drawCircle = () => {
+    if (!context) return;
+    const circleCenterX = CANVAS_WIDTH/2
+    const circleCenterY = CANVAS_HEIGHT - DOLY_POLY_MAX_RADIUS
+    const circleRadius = DOLY_POLY_MAX_RADIUS
+    context.arc(circleCenterX, circleCenterY, circleRadius, 0, Math.PI * 2)
+    context.lineWidth = 2;
+    context.stroke();
+}
+
 const init = () => {
     canvas.width = CANVAS_WIDTH
     canvas.height = CANVAS_HEIGHT
-    
+    drawCircle()
 }
 
 init()
